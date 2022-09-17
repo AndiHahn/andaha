189bfb6e4b8174a50384d7c08d948523e2397116
Andreas Hahn <andreas.hahn@dataformers.at>
2022-09-17 23:13:13 +0200

    fix identity service

--- src/CrossCutting/Andaha.CrossCutting.Application/Identity/IdentityService.cs
+++ src/CrossCutting/Andaha.CrossCutting.Application/Identity/IdentityService.cs
@@ -10,11 +10,7 @@ internal class IdentityService : IIdentityService
         _context = context ?? throw new ArgumentNullException(nameof(context));
     }
 
-    public Guid GetUserId() => new ("cfe4eb84-86ab-42f7-89a1-ca349dbbe085");
-
-    /*
     public Guid GetUserId()
         => new (_context.HttpContext?.User?.FindFirst("sub")?.Value ??
             throw new InvalidOperationException("No sub claim available."));
-    */
 }
